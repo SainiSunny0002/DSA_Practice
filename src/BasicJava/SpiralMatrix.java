98bc664c484a89174209c56727fd221280609072
SainiSunny0002 <s.suny0002@gmail.com>
2024-06-27 12:43:10 +0530

    DSA Practice

--- src/BasicJava/SpiralMatrix.java
+++ src/BasicJava/SpiralMatrix.java
@@ -36,10 +36,14 @@ public class SpiralMatrix {
     }
     public static void main(String[] args) {
         //Outhput: 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
-        int arr[][] = {{ 1, 2, 3, 4 },
-                       { 5, 6, 7, 8 },
-                       { 9, 10, 11, 12 },
-                       { 13, 14, 15, 16 }};
-        System.out.println(spiralOrder(arr));
+        Scanner cin = new Scanner(System.in);
+        int[][] matrix = new int[4][4];
+        for(int i=0;i< matrix.length;i++){
+            for (int j=0;j< matrix.length;j++){
+                matrix[i][j] = cin.nextInt();
+            }
+        }
+//                {{ 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 }}
+        System.out.println(spiralOrder(matrix));
     }
 }
