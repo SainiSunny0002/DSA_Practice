3c67a5d1072845fefaaae48cf4080a9f83cc80b4
SainiSunny0002 <s.suny0002@gmail.com>
2024-07-25 11:29:02 +0530

    DSA Practice

---
+++ src/BinarySearch/FindX_inSortedArray.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class FindX_inSortedArray {
+    // TC O(log2n) SC O(1)
+//    public static int findXInSortedArray(int arr[],int target){
+//        int low = 0,high = arr.length-1;
+//        while (low <= high){
+//            int mid = (low+high)/2;
+//            if (arr[mid] == target) return mid;
+//            else if(arr[mid] < target) low = mid+1;
+//            else high = mid-1;
+//        }
+//        return -1;
+//    }
+
+    //recursive approach
+    public static int findXInSortedArray(int arr[],int low,int high,int target){
+        if (low > high) return -1;
+        int mid = (low+high)/2;
+        if (arr[mid] == target) return mid;
+        else if(arr[mid] < target) return findXInSortedArray(arr,mid+1,high,target);
+        return findXInSortedArray(arr,low,mid-1,target);
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,7,9,11,12,45};
+        int target = 3;
+//        System.out.print(findXInSortedArray(arr,target));
+        System.out.print(findXInSortedArray(arr,0,arr.length-1,target));
+    }
+}
---
+++ src/BinarySearch/LowerBound.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class LowerBound {
+    // TC O(log2n) SC O(1)
+    public static int lowerBound(int arr[],int n,int x){
+        int low = 0,high = n-1;
+        int ans = n;
+        while (low <= high){
+            int mid = (low+high)/2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid-1;
+            }else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,3,7,8,9,9,9,11};
+        int n = arr.length;
+        int x = 1;
+        System.out.print(lowerBound(arr,n,x));
+    }
+}
